2555a74de9f6e2524fbca26e0e114f5086f95ba4
huska0707 <hussaink001haled@gmail.com>
2024-07-31 08:58:04 -0500

    get_page and format_name

--- src/crunchbaseScraper.py
+++ src/crunchbaseScraper.py
@@ -6,16 +6,17 @@ from PyQt5.QtWidgets import QApplication
 from PyQt5.QtCore import QUrl
 from PyQt5.QtWebEngineWidgets import QWebEnginePage
 
-BASE_URL = 'https://www.crunchbase.com'
+BASE_URL = "https://www.crunchbase.com"
 
 companies = []
 page = []
+
 
 class Page(QWebEnginePage):
     def __init__(self, url):
         self.app = QApplication(sys.argv)
         QWebEnginePage.__init__(self)
-        self.html = ''
+        self.html = ""
         self.loadFinished.connect(self._on_load_finished)
         self.load(QUrl(url))
         self.app.exec_()
@@ -26,3 +27,29 @@ class Page(QWebEnginePage):
     def Callable(self, html_str):
         self.html = html_str
         self.app.quit()
+
+
+def get_page(route):
+    if not route:
+        return None
+
+    try:
+        url = f"{BASE_URL}{route}"
+        pages.append(Page(url))
+        soup = BeautifulSoup(pages[-1].html, "lxml")
+        pages[-1].deleteLater()
+    except:
+        return None
+    else:
+        return soup
+
+
+def format_name(name):
+    return (
+        name.lower()
+        .replace("\n", "")
+        .strip()
+        .replace(".", "-")
+        .replace(" ", "-")
+        .replace(":", "-")
+    )
