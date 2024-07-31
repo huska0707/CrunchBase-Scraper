630d4aa3cc23c76912a59dd7142470d086dab977
huska0707 <hussaink001haled@gmail.com>
2024-07-31 09:12:36 -0500

    fixed errors in clipboardFetcher

--- src/clipboardFetcher.py
+++ src/clipboardFetcher.py
@@ -1,30 +1,37 @@
 import subprocess
 import threading
 import re
+import os
 
-def getClipboardData():
-    p = subprocess.Popen(["pbpaste"], stdout=subprocess.PIPE)
-    retcode = p.wait()
-    data = p.stdout.read()
-    return data.decode("utf-8")
 
-clip = getClipboardData()
+class ClipboardWatcher:
+    def __init__(self):
+        self.clip = self.get_clipboard_data()
 
-def check_for_clipboard_change():
-    global clip
+    def get_clipboard_data(self):
+        p = subprocess.Popen(["pbpaste"], stdout=subprocess.PIPE)
+        p.wait()
+        data = p.stdout.read()
+        return data.decode("utf-8")
 
-    threading.Timer(0.5, check_for_clipboard_change).start()
+    def check_for_clipboard_change(self):
+        threading.Timer(0.5, self.check_for_clipboard_change).start()
+        new_clip = self.get_clipboard_data()
 
-    clip2 = getClipboardData()
+        if self.clip != new_clip:
+            self.clip = new_clip
+            print("Clipboard changed")
 
-    if clip != clip2:
-        clip = clip2
-        print('clipboard changed')
+            match = re.findall(r"\d+\.\n(?:.*\n){3}(.*)", self.clip)
+            out = "\n".join(match)
 
-        match = re.findall(r'\d+\.\n(?:.*\n){3}(.*)', clip)
-        out = '\n'.join(match)
+            try:
+                with open("../data/list_of_company_names_raw.csv", "a") as file:
+                    file.write("\n" + out)
+            except Exception as e:
+                print(f"Error writing to file: {e}")
 
-        with open('../data/list_of_company_names_raw.csv', 'a') as file:
-            file.write('\n' + out)
 
-check_for_clipboard_change()
+if __name__ == "__main__":
+    watcher = ClipboardWatcher()
+    watcher.check_for_clipboard_change()
